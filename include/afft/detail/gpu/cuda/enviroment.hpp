93c4edcd4b0ce0cc6199eb759ca1f160a70d1bf8
David Bayer <bayer.david99@gmail.com>
2024-04-24 16:18:03 +0200

    #1: cuda enviroment module

---
+++ include/afft/detail/gpu/cuda/enviroment.hpp
@@ -0,0 +1,56 @@
+/*
+  This file is part of afft library.
+
+  Copyright (c) 2024 David Bayer
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef AFFT_DETAIL_GPU_CUDA_ENVIROMENT_HPP
+#define AFFT_DETAIL_GPU_CUDA_ENVIROMENT_HPP
+
+#include <string_view>
+
+// Check if the CUDA Toolkit root directory is defined
+#ifndef AFFT_GPU_CUDA_TOOLKIT_ROOT_DIR
+# error "CUDA Toolkit root directory is not defined"
+#endif
+
+namespace afft::detail::gpu::cuda
+{
+  /**
+   * @brief Get the root directory of the CUDA Toolkit.
+   * @return The root directory of the CUDA Toolkit.
+   */
+  [[nodiscard]] constexpr std::string_view getRootDir()
+  {
+    return {AFFT_GPU_CUDA_TOOLKIT_ROOT_DIR};
+  }
+
+  /**
+   * @brief Get the path to the CUDA Toolkit include directory.
+   * @return The path to the CUDA Toolkit include directory.
+   */
+  [[nodiscard]] constexpr std::string_view getIncludePath()
+  {
+    return {AFFT_GPU_CUDA_TOOLKIT_ROOT_DIR "/include"};
+  }
+} // namespace afft::detail::gpu::cuda
+
+#endif /* AFFT_DETAIL_GPU_CUDA_ENVIROMENT_HPP */
