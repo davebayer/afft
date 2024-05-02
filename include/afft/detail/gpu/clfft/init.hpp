1bf699cbe048f6ffbd3d017fdb99c4f2bec79470
David Bayer <bayer.david99@gmail.com>
2024-05-02 14:58:40 +0200

    #1: clfft init module

---
+++ include/afft/detail/gpu/clfft/init.hpp
@@ -0,0 +1,50 @@
+/*
+  This file is part of afft library.
+
+  Copyright (c) 2024 David Bayer
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef AFFT_DETAIL_GPU_CLFFT_INIT_HPP
+#define AFFT_DETAIL_GPU_CLFFT_INIT_HPP
+
+#include <clFFT.h>
+
+#include "../../../gpu.hpp"
+
+namespace afft::detail::gpu::clfft
+{
+  /// @brief Initialize the clFFT library.
+  inline void init(const afft::gpu::clfft::InitParameters&)
+  {
+    clfftSetupData clfftSetupData{};
+
+    Error::check(clfftInitSetupData(&clfftSetupData));
+    Error::check(clfftSetup(&clfftSetupData));
+  }
+
+  /// @brief Finalize the clFFT library.
+  inline void finalize()
+  {
+    Error::check(clfftTeardown());
+  }
+} // namespace afft::detail::gpu::clfft
+
+#endif /* AFFT_DETAIL_GPU_CLFFT_INIT_HPP */
