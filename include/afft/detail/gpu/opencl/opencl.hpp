8bb3f7e3a89e6141f286e3d37411c14d667b0358
David Bayer <bayer.david99@gmail.com>
2024-05-02 19:59:32 +0200

    #1: opencl opencl module

---
+++ include/afft/detail/gpu/opencl/opencl.hpp
@@ -0,0 +1,13 @@
+#ifndef AFFT_DETAIL_GPU_OPENCL_OPENCL_HPP
+#define AFFT_DETAIL_GPU_OPENCL_OPENCL_HPP
+
+#include "error.hpp"
+#include "include.hpp"
+#include "init.hpp"
+
+namespace afft::detail::gpu::opencl
+{
+
+} // namespace afft::detail::gpu::opencl
+
+#endif /* AFFT_DETAIL_GPU_OPENCL_OPENCL_HPP */
