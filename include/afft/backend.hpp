c9de239e9e54fb878125b1169442fac4705395e7
David Bayer <bayer.david99@gmail.com>
2024-05-12 10:48:30 +0200

    #1: backend module

---
+++ include/afft/backend.hpp
@@ -0,0 +1,129 @@
+/*
+  This file is part of afft library.
+
+  Copyright (c) 2024 David Bayer
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef AFFT_BACKEND_HPP
+#define AFFT_BACKEND_HPP
+
+#include <cstdint>
+#include <functional>
+#include <limits>
+#include <type_traits>
+
+#include "macro.hpp"
+#include "detail/backend.hpp"
+
+namespace afft
+{
+  /// @brief Backend for the FFT
+  enum class Backend : std::uint8_t
+  {
+    clfft,     ///< clFFT
+    cufft,     ///< cuFFT
+    fftw3,     ///< FFTW3
+    hipfft,    ///< hipFFT
+    mkl,       ///< Intel MKL
+    pocketfft, ///< PocketFFT
+    rocfft,    ///< rocFFT
+    vkfft,     ///< VkFFT
+  };
+
+  /// @brief Bitmask of backends
+  enum class BackendMask : std::uint16_t {};
+
+  /// @brief Backend select strategy
+  enum class BackendSelectStrategy : std::uint8_t
+  {
+    first, ///< select the first available backend
+    best,  ///< select the best available backend
+  };
+
+  /// @brief Value representing empty backend mask
+  inline constexpr BackendMask noBackend{};
+
+  /// @brief Value representing all backends
+  inline constexpr BackendMask allBackends{std::numeric_limits<BackendMask>::max()};
+
+  /**
+   * @brief Applies the bitwise `not` operation to a BackendMask or Backend.
+   * @tparam T Type of the value (Backend or BackendMask).
+   * @param value Value to apply the operation to.
+   * @return Result of the operation.
+   */
+  template<typename T>
+  [[nodiscard]] inline constexpr auto operator~(T value)
+    -> AFFT_RET_REQUIRES(BackendMask, (std::is_same_v<T, Backend> || std::is_same_v<T, BackendMask>))
+  {
+    return detail::backendMaskUnaryOp(std::bit_not<>{}, value);
+  }
+
+  /**
+   * @brief Applies the bitwise `and` operation to a BackendMask or Backend.
+   * @tparam T Type of the left-hand side (Backend or BackendMask).
+   * @tparam U Type of the right-hand side (Backend or BackendMask).
+   * @param lhs Left-hand side of the operation.
+   * @param rhs Right-hand side of the operation.
+   * @return Result of the operation.
+   */
+  template<typename T, typename U>
+  [[nodiscard]] inline constexpr auto operator&(T lhs, U rhs)
+    -> AFFT_RET_REQUIRES(BackendMask, (std::is_same_v<T, Backend> || std::is_same_v<T, BackendMask>) &&
+                                      (std::is_same_v<U, Backend> || std::is_same_v<U, BackendMask>))
+  {
+    return detail::backendMaskBinaryOp(std::bit_and<>{}, lhs, rhs);
+  }
+
+  /**
+   * @brief Applies the bitwise `or` operation to a BackendMask or Backend.
+   * @tparam T Type of the left-hand side (Backend or BackendMask).
+   * @tparam U Type of the right-hand side (Backend or BackendMask).
+   * @param lhs Left-hand side of the operation.
+   * @param rhs Right-hand side of the operation.
+   * @return Result of the operation.
+   */
+  template<typename T, typename U>
+  [[nodiscard]] inline constexpr auto operator|(T lhs, U rhs)
+    -> AFFT_RET_REQUIRES(BackendMask, (std::is_same_v<T, Backend> || std::is_same_v<T, BackendMask>) &&
+                                      (std::is_same_v<U, Backend> || std::is_same_v<U, BackendMask>))
+  {
+    return detail::backendMaskBinaryOp(std::bit_or<>{}, lhs, rhs);
+  }
+
+  /**
+   * @brief Applies the bitwise `xor` operation to a BackendMask or Backend.
+   * @tparam T Type of the left-hand side (Backend or BackendMask).
+   * @tparam U Type of the right-hand side (Backend or BackendMask).
+   * @param lhs Left-hand side of the operation.
+   * @param rhs Right-hand side of the operation.
+   * @return Result of the operation.
+   */
+  template<typename T, typename U>
+  [[nodiscard]] inline constexpr auto operator^(T lhs, U rhs)
+    -> AFFT_RET_REQUIRES(BackendMask, (std::is_same_v<T, Backend> || std::is_same_v<T, BackendMask>) &&
+                                      (std::is_same_v<U, Backend> || std::is_same_v<U, BackendMask>))
+  {
+    return detail::backendMaskBinaryOp(std::bit_xor<>{}, lhs, rhs);
+  }
+} // namespace afft
+
+#endif /* AFFT_BACKEND_HPP */
---
+++ include/afft/detail/backend.hpp
@@ -0,0 +1,104 @@
+/*
+  This file is part of afft library.
+
+  Copyright (c) 2024 David Bayer
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef AFFT_DETAIL_BACKEND_HPP
+#define AFFT_DETAIL_BACKEND_HPP
+
+#include <cstdint>
+#include <type_traits>
+
+#include "cxx.hpp"
+
+namespace afft
+{
+  // Forward declarations
+  enum class Backend : std::uint8_t;
+  enum class BackendMask : std::uint16_t;
+
+namespace detail
+{
+  /**
+   * @brief Converts Backend or BackednMask to a BackendMask.
+   * @tparam T Type of the value.
+   * @param value Value to convert.
+   * @return BackendMask representation of the value.
+   */
+  template<typename T>
+  [[nodiscard]] inline constexpr BackendMask toBackendMask(T value)
+  {
+    static_assert(std::is_same_v<T, Backend> || std::is_same_v<T, BackendMask>,
+                  "T must be either Backend or BackendMask");
+
+    if constexpr (std::is_same_v<T, Backend>)
+    {
+      return static_cast<BackendMask>(U{1} << detail::cxx::to_underlying(value));
+    }
+    else
+    {
+      return value;
+    }
+  }
+
+  /**
+   * @brief Applies a unary operation to a BackendMask.
+   * @tparam UnOp Type of the unary operation.
+   * @tparam T Type of the value.
+   * @param fn Unary operation to apply.
+   * @param value Value to apply the operation to.
+   * @return Result of the operation.
+   */
+  template<typename UnOp, typename T>
+  [[nodiscard]] inline constexpr BackendMask backendMaskUnaryOp(UnOp fn, T value)
+  {
+    using U = std::underlying_type_t<BackendMask>;
+
+    U val = detail::cxx::to_underlying(detail::toBackendMask(value));
+
+    return BackendMask{fn(val)};
+  }
+
+  /**
+   * @brief Applies a binary operation to two BackendMask values.
+   * @tparam BinFn Type of the binary operation.
+   * @tparam T Type of the left-hand side value.
+   * @tparam U Type of the right-hand side value.
+   * @param fn Binary operation to apply.
+   * @param lhs Left-hand side value.
+   * @param rhs Right-hand side value.
+   * @return Result of the operation.
+   */
+  template<typename BinFn, typename T, typename U>
+  [[nodiscard]] inline constexpr BackendMask backendMaskBinaryOp(BinFn fn, T lhs, U rhs)
+  {
+    using U = std::underlying_type_t<BackendMask>;
+
+    U left  = detail::cxx::to_underlying(detail::toBackendMask(lhs));
+    U right = detail::cxx::to_underlying(detail::toBackendMask(rhs));
+
+    return BackendMask{fn(left, right)};
+  }
+} // namespace detail
+} // namespace afft
+
+#endif /* AFFT_DETAIL_BACKEND_HPP */
