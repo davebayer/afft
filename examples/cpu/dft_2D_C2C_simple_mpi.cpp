114d6e20ea2c2136ee5ca4a063e2c90a50838f57
David Bayer <bayer.david99@gmail.com>
2024-05-13 12:51:08 +0200

    #1: replace cpu distrib example by cpu mpi example

--- examples/cpu/dft_2D_C2C_simple_mpi.cpp
+++ examples/cpu/dft_2D_C2C_simple_mpi.cpp
@@ -26,16 +26,13 @@ int main(void)
   dftParams.type             = afft::dft::Type::complexToComplex; // let's use complex-to-complex transform
   dftParams.normalization    = afft::Normalization::orthogonal; // use orthogonal normalization
 
-  afft::distrib::cpu::Parameters<afft::distrib::Implementation::mpi> mpiCpuParams{}; // it will run on multiple CPUs using MPI implementation
-  mpiCpuParams.communicator  = MPI_COMM_WORLD; // set up the communicator (unnecessary, MPI_COMM_WORLD is chosen as the default communicator)
+  afft::mpi::cpu::Parameters mpiCpuParams{}; // it will run on multiple CPUs using MPI implementation
+  mpiCpuParams.multiProcessParameters  = MPI_COMM_WORLD; // set up the communicator (unnecessary, MPI_COMM_WORLD is chosen as the default communicator)
   mpiCpuParams.destroySource = true; // allow to destroy source data
   mpiCpuParams.alignment     = afft::cpu::AlignedAllocator<>{}.getAlignment(); // get alignment of the pointers
   mpiCpuParams.threadLimit   = 4; // we will use up to 4 threads per MPI process
 
   auto plan = afft::makePlan(dftParams, mpiCpuParams); // generate the plan of the transform, also sets up the memory layout in cpuParams
-
-  const auto [srcElemCount, _placeHolder1] = plan.getSrcBufferElemCount(); // get the required size of the source buffer (may be greater than the volume of the source data)
-  const auto [dstElemCount, _placeHolder2] = plan.getDstBufferElemCount(); // get the required size of the destination buffer (may be greater than the volume of the destination data)
 
   src.resize(srcElemCount); // resize the source vector
   dst.resize(dstElemCount); // resize the destination vector
