a0114594fec020738027bee9fa8328992d0b648d
David Bayer <bayer.david99@gmail.com>
2024-06-03 12:14:40 +0200

    #1: base of Desc module

---
+++ include/afft/detail/Desc.hpp
@@ -0,0 +1,72 @@
+/*
+  This file is part of afft library.
+
+  Copyright (c) 2024 David Bayer
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef AFFT_DETAIL_DESC_HPP
+#define AFFT_DETAIL_DESC_HPP
+
+#ifndef AFFT_TOP_LEVEL_INCLUDE
+# include "include.hpp"
+#endif
+
+#include "ArchDesc.hpp"
+#include "TransformDesc.hpp"
+
+namespace afft::detail
+{
+  class Desc : public TransformDesc, public ArchDesc
+  {
+    public:
+      /// @brief Default constructor is deleted.
+      Desc() = delete;
+
+      /// @brief Constructor.
+      template<typename TransformParametersT, typename ArchParametersT>
+      Desc(const TransformParametersT& transformParameters, const ArchParametersT& archParameters)
+      : TransformDesc{transformParameters},
+        ArchDesc{archParameters}
+      {
+        static_assert(isTransformParameters<TransformParametersT>, "TransformParametersT must be a TransformParameters type.");
+        static_assert(isArchitectureParameters<ArchParametersT>, "ArchParametersT must be an ArchParameters type.");
+      }
+
+      /// @brief Copy constructor.
+      Desc(const Desc&) = default;
+
+      /// @brief Move constructor.
+      Desc(Desc&&) = default;
+
+      /// @brief Destructor.
+      ~Desc() = default;
+
+      /// @brief Copy assignment operator.
+      Desc& operator=(const Desc&) = default;
+
+      /// @brief Move assignment operator.
+      Desc& operator=(Desc&&) = default;
+    private:
+
+  };
+} // namespace afft::detail
+
+#endif /* AFFT_DETAIL_DESC_HPP */
