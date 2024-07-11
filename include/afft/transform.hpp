061c8e5ec4c00fb725205de93568d275cb8fcaeb
David Bayer <bayer.david99@gmail.com>
2024-07-11 15:48:05 +0200

    #1: refactor transform module

--- include/afft/transform.hpp
+++ include/afft/transform.hpp
@@ -30,127 +30,112 @@
 #endif
 
 #include "common.hpp"
-#include "detail/transform.hpp"
 
 AFFT_EXPORT namespace afft
 {
   /// @brief Named constant representing all axes (is empty view)
-  template<std::size_t transformExt = dynamicRank>
-  inline constexpr View<std::size_t, transformExt> allAxes{};
+  inline constexpr View<Axis> allAxes{};
 
-  /// @brief Namespace for discrete Fourier transform
+/**********************************************************************************************************************/
+// Discrete Fourier Transform (DFT)
+/**********************************************************************************************************************/
   namespace dft
   {
-    /// @brief DFT transform type
-    enum class Type : std::uint8_t
-    {
-      complexToComplex,       ///< complex-to-complex transform
-      realToComplex,          ///< real-to-complex transform
-      complexToReal,          ///< complex-to-real transform
-
-      c2c = complexToComplex, ///< alias for complex-to-complex transform
-      r2c = realToComplex,    ///< alias for real-to-complex transform
-      c2r = complexToReal,    ///< alias for complex-to-real transform
-    };
-
-    /**
-     * @brief DFT Parameters
-     * @tparam shapeExt Extent of the shape, dynamic by default
-     * @tparam transformExt Extent of the transform, dynamic by default
-     */
-    template<std::size_t shapeExt = dynamicExtent, std::size_t transformExt = dynamicExtent>
-    struct Parameters : detail::TransformParametersBase<shapeExt, transformExt>
-    {
-      static_assert((shapeExt == dynamicExtent) || (shapeExt > 0), "shape extent must be greater than 0");
-      static_assert((transformExt == dynamicExtent) || (transformExt > 0), "transform extent must be greater than 0");
-      static_assert((shapeExt == dynamicExtent) || (transformExt == dynamicExtent) || (transformExt <= shapeExt),
-                    "transform rank must be less than or equal to shape rank");
-
-      Direction                       direction{};                        ///< direction of the transform
-      PrecisionTriad                  precision{};                        ///< precision triad
-      View<std::size_t, shapeExt>     shape{};                            ///< shape of the transform
-      View<std::size_t, transformExt> axes{allAxes<transformExt>};        ///< axes of the transform
-      Normalization                   normalization{Normalization::none}; ///< normalization
-      Placement                       placement{Placement::outOfPlace};   ///< placement of the transform
-      Type                            type{Type::complexToComplex};       ///< type of the transform
-    };
+    enum class Type : std::uint8_t;
+    struct Parameters;
   } // namespace dft
 
-  /// @brief Namespace for discrete Hartley transform
+  /// @brief DFT transform type
+  enum class dft::Type : std::uint8_t
+  {
+    complexToComplex,       ///< complex-to-complex transform
+    realToComplex,          ///< real-to-complex transform
+    complexToReal,          ///< complex-to-real transform
+
+    c2c = complexToComplex, ///< alias for complex-to-complex transform
+    r2c = realToComplex,    ///< alias for real-to-complex transform
+    c2r = complexToReal,    ///< alias for complex-to-real transform
+  };
+
+  /// @brief DFT Parameters
+  struct dft::Parameters
+  {
+    Direction      direction{};                                  ///< direction of the transform
+    PrecisionTriad precision{};                                  ///< precision triad
+    View<Size>     shape{};                                      ///< shape of the transform
+    View<Axis>     axes{allAxes};                                ///< axes of the transform
+    Normalization  normalization{Normalization::none};           ///< normalization
+    Placement      placement{Placement::outOfPlace};             ///< placement of the transform
+    bool           destructive{placement == Placement::inPlace}; ///< destructive transform
+    Type           type{Type::complexToComplex};                 ///< type of the transform
+  };
+
+/**********************************************************************************************************************/
+// Discrete Hartley Transform (DHT)
+/**********************************************************************************************************************/
   namespace dht
   {
-    /// @brief DHT transform type
-    enum class Type : std::uint8_t
-    {
-      separable, ///< separable DHT, computes the DHT along each axis independently
-    };
-
-    /**
-     * @brief DHT Parameters
-     * @tparam shapeExt Extent of the shape, dynamic by default
-     * @tparam transformExt Extent of the transform, dynamic by default
-     */
-    template<std::size_t shapeExt = dynamicExtent, std::size_t transformExt = dynamicExtent>
-    struct Parameters : detail::TransformParametersBase<shapeExt, transformExt>
-    {
-      static_assert((shapeExt == dynamicExtent) || (shapeExt > 0), "shape rank must be greater than 0");
-      static_assert((transformExt == dynamicExtent) || (transformExt > 0), "transform rank must be greater than 0");
-      static_assert((shapeExt == dynamicExtent) || (transformExt == dynamicExtent) || (transformExt <= shapeExt),
-                    "transform rank must be less than or equal to shape rank");
-
-      Direction                       direction{};                        ///< direction of the transform
-      PrecisionTriad                  precision{};                        ///< precision triad
-      View<std::size_t, shapeExt>     shape{};                            ///< shape of the transform
-      View<std::size_t, transformExt> axes{allAxes<transformExt>};        ///< axes of the transform
-      Normalization                   normalization{Normalization::none}; ///< normalization
-      Placement                       placement{Placement::outOfPlace};   ///< placement of the transform
-      Type                            type{Type::separable};              ///< type of the transform
-    };
+    enum class Type : std::uint8_t;
+    struct Parameters;
   } // namespace dht
 
-  /// @brief Namespace for discrete trigonometric transform
+  /// @brief DHT transform type
+  enum class dht::Type : std::uint8_t
+  {
+    separable, ///< separable DHT, computes the DHT along each axis independently
+  };
+
+  /// @brief DHT Parameters
+  struct dht::Parameters
+  {
+    Direction      direction{};                                  ///< direction of the transform
+    PrecisionTriad precision{};                                  ///< precision triad
+    View<Size>     shape{};                                      ///< shape of the transform
+    View<Axis>     axes{allAxes};                                ///< axes of the transform
+    Normalization  normalization{Normalization::none};           ///< normalization
+    Placement      placement{Placement::outOfPlace};             ///< placement of the transform
+    bool           destructive{placement == Placement::inPlace}; ///< destructive transform
+    Type           type{Type::separable};                        ///< type of the transform
+  };
+
+/**********************************************************************************************************************/
+// Discrete Trigonomic Transform (DTT)
+/**********************************************************************************************************************/
   namespace dtt
   {
-    /// @brief DTT transform type
-    enum class Type : std::uint8_t
-    {
-      dct1,       ///< Discrete Cosine Transform type I
-      dct2,       ///< Discrete Cosine Transform type II
-      dct3,       ///< Discrete Cosine Transform type III
-      dct4,       ///< Discrete Cosine Transform type IV
-
-      dst1,       ///< Discrete Sine Transform type I
-      dst2,       ///< Discrete Sine Transform type II
-      dst3,       ///< Discrete Sine Transform type III
-      dst4,       ///< Discrete Sine Transform type IV
-
-      dct = dct2, ///< default DCT type
-      dst = dst2, ///< default DST type
-    };
-
-    /**
-     * @brief DTT Parameters
-     * @tparam shapeExt Extent of the shape, dynamic by default
-     * @tparam transformExt Extent of the transform, dynamic by default
-     */
-    template<std::size_t shapeExt     = dynamicExtent,
-             std::size_t transformExt = dynamicExtent>
-    struct Parameters : detail::TransformParametersBase<shapeExt, transformExt>
-    {
-      static_assert((shapeExt == dynamicExtent) || (shapeExt > 0), "shape rank must be greater than 0");
-      static_assert((transformExt == dynamicExtent) || (transformExt > 0), "transform rank must be greater than 0");
-      static_assert((shapeExt == dynamicExtent) || (transformExt == dynamicExtent) || (transformExt <= shapeExt),
-                    "transform rank must be less than or equal to shape rank");
-
-      Direction                       direction{};                        ///< direction of the transform
-      PrecisionTriad                  precision{};                        ///< precision triad
-      View<std::size_t, shapeExt>     shape{};                            ///< shape of the transform
-      View<std::size_t, transformExt> axes{allAxes<transformExt>};        ///< axes of the transform
-      Normalization                   normalization{Normalization::none}; ///< normalization
-      Placement                       placement{Placement::outOfPlace};   ///< placement of the transform
-      View<Type>                      types{};                            ///< types of the transform, must have size 1 or size equal to the number of axes
-    };
+    enum class Type : std::uint8_t;
+    struct Parameters;
   } // namespace dtt
+
+  /// @brief DTT transform type
+  enum class dtt::Type : std::uint8_t
+  {
+    dct1,       ///< Discrete Cosine Transform type I
+    dct2,       ///< Discrete Cosine Transform type II
+    dct3,       ///< Discrete Cosine Transform type III
+    dct4,       ///< Discrete Cosine Transform type IV
+
+    dst1,       ///< Discrete Sine Transform type I
+    dst2,       ///< Discrete Sine Transform type II
+    dst3,       ///< Discrete Sine Transform type III
+    dst4,       ///< Discrete Sine Transform type IV
+
+    dct = dct2, ///< default DCT type
+    dst = dst2, ///< default DST type
+  };
+
+  /// @brief DTT Parameters
+  struct dtt::Parameters
+  {
+    Direction      direction{};                                  ///< direction of the transform
+    PrecisionTriad precision{};                                  ///< precision triad
+    View<Size>     shape{};                                      ///< shape of the transform
+    View<Axis>     axes{allAxes};                                ///< axes of the transform
+    Normalization  normalization{Normalization::none};           ///< normalization
+    Placement      placement{Placement::outOfPlace};             ///< placement of the transform
+    bool           destructive{placement == Placement::inPlace}; ///< destructive transform
+    View<Type>     types{};                                      ///< types of the transform, must have size 1 or size equal to the number of axes
+  };
 } // namespace afft
 
 #endif /* AFFT_TRANSFORM_HPP */
