8fce7213027fcc4d49f1c23ea93ea5f448b7e995
David Bayer <bayer.david99@gmail.com>
2024-07-17 10:32:56 +0200

    #1: examples hip helpers

---
+++ examples/helpers/hip.hpp
@@ -0,0 +1,91 @@
+/*
+  This file is part of afft library.
+
+  Copyright (c) 2024 David Bayer
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef HELPERS_HIP_HPP
+#define HELPERS_HIP_HPP
+
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+#include <hip_runtime.h>
+
+/**
+ * @brief Macro for checking HIP errors. The call cannot contain _error variable.
+ * @param call HIP function call
+ */
+#define HIP_CALL(call) \
+  do { \
+    const hipError_t _error = (call); \
+    if (_error != hipSuccess) \
+    { \
+      std::fprintf(stderr, "HIP error (%s:%d): %s\n", __FILE__, __LINE__, hipGetErrorString(_error)); \
+      std::exit(EXIT_FAILURE); \
+    } \
+  } while (0)
+
+namespace helpers::hip
+{
+  /**
+   * @brief Get the number of HIP devices
+   * @return Number of HIP devices
+   */
+  inline int getDeviceCount()
+  {
+    int deviceCount{};
+
+    HIP_CALL(hipGetDeviceCount(&deviceCount));
+
+    return deviceCount;
+  }
+
+  /// @brief Deleter for HIP streams
+  struct StreamDeleter
+  {
+    void operator()(hipStream_t stream) const
+    {
+      hipStreamDestroy(stream);
+    }
+  };
+
+  /// @brief Unique pointer for HIP streams
+  using Stream = std::unique_ptr<std::remove_pointer_t<hipStream_t>, StreamDeleter>;
+
+  /**
+   * @brief Make a HIP stream
+   * @return HIP stream
+   */
+  inline Stream makeStream() noexcept
+  {
+    hipStream_t stream{};
+
+    HIP_CALL(hipStreamCreate(&stream));
+
+    return Stream{std::move(stream)};
+  }
+} // namespace helpers::hip
+
+#endif /* HELPERS_HIP_HPP */
