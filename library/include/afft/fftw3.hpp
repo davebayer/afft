3c392dc35b6fb54ccbae1091bfa02284991c633d
David Bayer <bayer.david99@gmail.com>
2024-08-21 10:48:10 +0200

    #1: fix fftw3 module compilation

--- library/include/afft/fftw3.hpp
+++ library/include/afft/fftw3.hpp
@@ -46,7 +46,113 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to export the wisdom from.
    * @param filename Name of the file to export the wisdom to.
    */
-  void exportWisdomToFilename(Library library, [[maybe_unused]] const char* filename)
+  void exportWisdomToFilename(Library library, const char* filename);
+
+  /**
+   * @brief Export FFTW3 wisdom to a file.
+   * @param library Library to export the wisdom from.
+   * @param filename Name of the file to export the wisdom to.
+   */
+  void exportWisdomToFilename(Library library, std::string_view filename);
+
+  /**
+   * @brief Export FFTW3 wisdom to a file.
+   * @param library Library to export the wisdom from.
+   * @param file File to export the wisdom to.
+   */
+  void exportWisdomToFile(Library library, FILE* file);
+
+  /**
+   * @brief Export FFTW3 wisdom to a string.
+   * @param library Library to export the wisdom from.
+   * @return String containing the wisdom. If the Library is not supported, an empty unique pointer is returned.
+   */
+  [[nodiscard]] std::unique_ptr<char[], FreeDeleter> exportWisdomToString(Library library);
+
+  /**
+   * @brief Export FFTW3 wisdom to a std::string.
+   * @param library Library to export the wisdom from.
+   * @return std::string containing the wisdom. If the Library is not supported, an empty string.
+   */
+  [[nodiscard]] std::string exportWisdomToStdString(Library library);
+
+  /**
+   * @brief Import FFTW3 wisdom from the system. Only on Unix and GNU systems.
+   * @param library Library to import the wisdom to.
+   */
+  void importSystemWisdom(Library library);
+
+  /**
+   * @brief Import FFTW3 wisdom from a file.
+   * @param library Library to import the wisdom to.
+   * @param filename Name of the file to import the wisdom from.
+   */
+  void importWisdomFromFilename(Library library, const char* filename);
+
+  /**
+   * @brief Import FFTW3 wisdom from a file.
+   * @param filename Name of the file to import the wisdom from.
+   * @param library Library to import the wisdom to.
+   */
+  void importWisdomFromFilename(Library library, std::string_view filename);
+
+  /**
+   * @brief Import FFTW3 wisdom from a file.
+   * @param library Library to import the wisdom to.
+   * @param file File to import the wisdom from.
+   */
+  void importWisdomFromFile(Library library, FILE* file);
+
+  /**
+   * @brief Import FFTW3 wisdom from a string.
+   * @param library Library to import the wisdom to.
+   * @param wisdom String containing the wisdom.
+   */
+  void importWisdomFromString(Library library, const char* wisdom);
+
+  /**
+   * @brief Import FFTW3 wisdom from a string.
+   * @param library Library to import the wisdom to.
+   * @param wisdom String containing the wisdom.
+   */
+  void importWisdomFromString(Library library, std::string_view wisdom);
+
+  /**
+   * @brief Forget all FFTW3 wisdom.
+   * @param library Library to forget the wisdom.
+   */
+  void forgetWisdom(Library library);
+
+#ifdef AFFT_ENABLE_MPI
+  namespace mpi
+  {
+    /**
+     * @brief Broadcast FFTW3 wisdom to all MPI processes from the root process.
+     * @param library Library to be the wisdom broadcasted for.
+     * @param comm MPI communicator.
+     */
+    void broadcastWisdom(Library library, MPI_Comm comm);
+
+    /**
+     * @brief Gather FFTW3 wisdom from all MPI processes to the root process.
+     * @param library Library to be the wisdom gathered for.
+     * @param comm MPI communicator.
+     */
+    void gatherWisdom(Library library, MPI_Comm comm);
+  } // namespace mpi
+#endif
+} // namespace afft::fftw3
+
+#ifdef AFFT_HEADER_ONLY
+
+AFFT_EXPORT namespace afft::fftw3
+{
+  /**
+   * @brief Export FFTW3 wisdom to a file.
+   * @param library Library to export the wisdom from.
+   * @param filename Name of the file to export the wisdom to.
+   */
+  AFFT_HEADER_ONLY_INLINE void exportWisdomToFilename(Library library, [[maybe_unused]] const char* filename)
   {
     detail::validate(library);
 
@@ -92,7 +198,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to export the wisdom from.
    * @param filename Name of the file to export the wisdom to.
    */
-  void exportWisdomToFilename(Library library, std::string_view filename)
+  AFFT_HEADER_ONLY_INLINE void exportWisdomToFilename(Library library, std::string_view filename)
   {
     exportWisdomToFilename(library, filename.data());
   }
@@ -102,7 +208,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to export the wisdom from.
    * @param file File to export the wisdom to.
    */
-  void exportWisdomToFile(Library library, [[maybe_unused]] FILE* file)
+  AFFT_HEADER_ONLY_INLINE void exportWisdomToFile(Library library, [[maybe_unused]] FILE* file)
   {
     detail::validate(library);
 
@@ -140,7 +246,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to export the wisdom from.
    * @return String containing the wisdom. If the Library is not supported, an empty unique pointer is returned.
    */
-  [[nodiscard]] std::unique_ptr<char[], FreeDeleter> exportWisdomToString(Library library)
+  [[nodiscard]] AFFT_HEADER_ONLY_INLINE std::unique_ptr<char[], FreeDeleter> exportWisdomToString(Library library)
   {
     detail::validate(library);
 
@@ -187,7 +293,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to export the wisdom from.
    * @return std::string containing the wisdom. If the Library is not supported, an empty string.
    */
-  [[nodiscard]] std::string exportWisdomToStdString(Library library)
+  [[nodiscard]] AFFT_HEADER_ONLY_INLINE std::string exportWisdomToStdString(Library library)
   {
     auto wisdom = exportWisdomToString(library);
 
@@ -205,7 +311,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @brief Import FFTW3 wisdom from the system. Only on Unix and GNU systems.
    * @param library Library to import the wisdom to.
    */
-  void importSystemWisdom(Library library)
+  AFFT_HEADER_ONLY_INLINE void importSystemWisdom(Library library)
   {
     detail::validate(library);
 
@@ -251,7 +357,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to import the wisdom to.
    * @param filename Name of the file to import the wisdom from.
    */
-  void importWisdomFromFilename(Library library, [[maybe_unused]] const char* filename)
+  AFFT_HEADER_ONLY_INLINE void importWisdomFromFilename(Library library, [[maybe_unused]] const char* filename)
   {
     detail::validate(library);
 
@@ -297,7 +403,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param filename Name of the file to import the wisdom from.
    * @param library Library to import the wisdom to.
    */
-  void importWisdomFromFilename(Library library, std::string_view filename)
+  AFFT_HEADER_ONLY_INLINE void importWisdomFromFilename(Library library, std::string_view filename)
   {
     importWisdomFromFilename(library, filename.data());
   }
@@ -307,7 +413,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to import the wisdom to.
    * @param file File to import the wisdom from.
    */
-  void importWisdomFromFile(Library library, [[maybe_unused]] FILE* file)
+  AFFT_HEADER_ONLY_INLINE void importWisdomFromFile(Library library, [[maybe_unused]] FILE* file)
   {
     detail::validate(library);
 
@@ -353,7 +459,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to import the wisdom to.
    * @param wisdom String containing the wisdom.
    */
-  void importWisdomFromString(Library library, [[maybe_unused]] const char* wisdom)
+  AFFT_HEADER_ONLY_INLINE void importWisdomFromString(Library library, [[maybe_unused]] const char* wisdom)
   {
     detail::validate(library);
 
@@ -399,7 +505,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to import the wisdom to.
    * @param wisdom String containing the wisdom.
    */
-  void importWisdomFromString(Library library, std::string_view wisdom)
+  AFFT_HEADER_ONLY_INLINE void importWisdomFromString(Library library, std::string_view wisdom)
   {
     importWisdomFromString(library, wisdom.data());
   }
@@ -408,7 +514,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @brief Forget all FFTW3 wisdom.
    * @param library Library to forget the wisdom.
    */
-  void forgetWisdom(Library library)
+  AFFT_HEADER_ONLY_INLINE void forgetWisdom(Library library)
   {
     detail::validate(library);
 
@@ -442,29 +548,12 @@ AFFT_EXPORT namespace afft::fftw3
   }
 
 #ifdef AFFT_ENABLE_MPI
-  namespace mpi
-  {
-    /**
-     * @brief Broadcast FFTW3 wisdom to all MPI processes from the root process.
-     * @param library Library to be the wisdom broadcasted for.
-     * @param comm MPI communicator.
-     */
-    void broadcastWisdom(Library library, MPI_Comm comm);
-
-    /**
-     * @brief Gather FFTW3 wisdom from all MPI processes to the root process.
-     * @param library Library to be the wisdom gathered for.
-     * @param comm MPI communicator.
-     */
-    void gatherWisdom(Library library, MPI_Comm comm);
-  } // namespace mpi
-
   /**
    * @brief Broadcast FFTW3 wisdom to all MPI processes from the root process.
    * @param library Library to be the wisdom broadcasted for.
    * @param comm MPI communicator.
    */
-  void mpi::broadcastWisdom(Library library, [[maybe_unused]] MPI_Comm comm)
+  AFFT_HEADER_ONLY_INLINE void mpi::broadcastWisdom(Library library, [[maybe_unused]] MPI_Comm comm)
   {
     detail::validate(library);
 
@@ -497,7 +586,7 @@ AFFT_EXPORT namespace afft::fftw3
    * @param library Library to be the wisdom gathered for.
    * @param comm MPI communicator.
    */
-  void mpi::gatherWisdom(Library library, [[maybe_unused]] MPI_Comm comm)
+  AFFT_HEADER_ONLY_INLINE void mpi::gatherWisdom(Library library, [[maybe_unused]] MPI_Comm comm)
   {
     detail::validate(library);
 
@@ -525,6 +614,8 @@ AFFT_EXPORT namespace afft::fftw3
     }
   }
 #endif
-} // namespace afft
+} // namespace afft::fftw3
+
+#endif /* AFFT_HEADER_ONLY */
 
 #endif /* AFFT_FFTW3_HPP */
