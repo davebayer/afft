bcaf79dc8180b287aa9943e41349d22982c3bc6c
David Bayer <bayer.david99@gmail.com>
2024-08-29 10:50:15 +0200

    #1: `PlanCache` implementation

--- library/include/afft/PlanCache.hpp
+++ library/include/afft/PlanCache.hpp
@@ -30,118 +30,51 @@
 #endif
 
 #include "common.hpp"
+#include "Description.hpp"
 #include "error.hpp"
 #include "Plan.hpp"
 #include "detail/Desc.hpp"
 
 AFFT_EXPORT namespace afft
 {
+  /// @brief Plan cache.
   class PlanCache
   {
     private:
-      using ListValue = std::shared_ptr<Plan>;                      ///< The value type of the cache.
-      using List      = std::list<ListValue>;                       ///< The list type of the cache.
-      using ListIter  = List::iterator;                             ///< The list iterator type of the cache.
+      using ListValue     = std::shared_ptr<Plan>;                                   ///< The value type of the cache.
+      using List          = std::list<ListValue>;                                    ///< The list type of the cache.
+      using ListIter      = List::iterator;                                          ///< The list iterator type of the cache.
+      using ListConstIter = List::const_iterator;                                    ///< The list const iterator type of the cache.
 
-      using MapKey    = std::reference_wrapper<const detail::Desc>;              ///< The key type of the cache.
-      using MapValue  = ListIter;                                                ///< The value type of the map.
-      using MapHash   = struct MapHashImpl;                                      ///< The hash function of the map.
-      using MapEqual  = std::equal_to<detail::Desc>;                             ///< The equality function of the map.
-      using Map       = std::unordered_map<MapKey, MapValue, MapHash, MapEqual>; ///< The map type of the cache.
-      using MapIter   = Map::iterator;                                           ///< The map iterator type of the cache;
-
-      struct MapHashImpl
+      using MapKey        = std::reference_wrapper<const Description>;               ///< The key type of the cache.
+      using MapValue      = ListIter;                                                ///< The value type of the map.
+      struct MapHash                                                                 ///< The hash function of the map.
       {
         [[nodiscard]] std::size_t operator()(MapKey key) const noexcept
         {
-          return std::hash<detail::Desc>{}(key.get());
+          return std::hash<Description>{}(key);
         }
       };
+      using MapEqual      = std::equal_to<Description>;                              ///< The equality function of the map.
+      using Map           = std::unordered_map<MapKey, MapValue, MapHash, MapEqual>; ///< The map type of the cache.
+      using MapIter       = Map::iterator;                                           ///< The map iterator type of the cache;
+      using MapConstIter  = Map::const_iterator;                                     ///< The map const iterator type of the cache;
+      
     public:
-      // Forward declarations
-      class Iterator;
-
-      using value_type             = Plan*;                                 ///< The value type of the cache.
-      using size_type              = std::size_t;                           ///< The size type of the cache.
-      using difference_type        = std::ptrdiff_t;                        ///< The difference type of the cache.
-      using reference	             = value_type&;                           ///< The reference type of the cache.
-      using const_reference        = const value_type&;                     ///< The const reference type of the cache.
-      using pointer                = value_type*;                           ///< The pointer type of the cache.            
-      using const_pointer          = const value_type*;                     ///< The const pointer type of the cache.
-      using iterator               = Iterator;                              ///< The iterator type of the cache.
-      using const_iterator         = const Iterator;                        ///< The const iterator type of the cache.
-      using reverse_iterator       = std::reverse_iterator<iterator>;       ///< The reverse iterator type of the cache.
-      using const_reverse_iterator = std::reverse_iterator<const_iterator>; ///< The const reverse iterator type of the cache.
-
-      class Iterator
-      {
-      public:
-        using iterator_category = std::forward_iterator_tag;
-        using value_type        = Plan;
-        using difference_type   = std::ptrdiff_t;
-        using pointer           = Plan*;
-        using reference         = Plan&;
-
-        Iterator() = default;
-
-        Iterator(const Iterator&) = default;
-
-        Iterator(Iterator&&) = default;
-
-        ~Iterator() = default;
-
-        Iterator& operator=(const Iterator&) = default;
-
-        Iterator& operator=(Iterator&&) = default;
-
-        reference operator*() const
-        {
-          return **mIter;
-        }
-
-        pointer operator->() const
-        {
-          return mIter->get();
-        }
-
-        Iterator& operator++()
-        {
-          ++mIter;
-          return *this;
-        }
-
-        Iterator operator++(int)
-        {
-          auto temp = *this;
-          ++mIter;
-          return temp;
-        }
-
-        friend bool operator==(const Iterator& lhs, const Iterator& rhs)
-        {
-          return lhs.mIter == rhs.mIter;
-        }
-
-        friend bool operator!=(const Iterator& lhs, const Iterator& rhs)
-        {
-          return lhs.mIter != rhs.mIter;
-        }
-
-      protected:
-      private:
-        friend class PlanCache;
-
-        using ListIter = PlanCache::ListIter;
-
-        explicit iterator(ListIter iter)
-        : mIter{iter}
-        {}
-
-        ListIter mIter{};
-    };
+      using value_type             = List::value_type;             ///< The value type of the cache.
+      using size_type              = List::size_type;              ///< The size type of the cache.
+      using difference_type        = List::difference_type;        ///< The difference type of the cache.
+      using reference	             = List::reference;              ///< The reference type of the cache.
+      using const_reference        = List::const_reference;        ///< The const reference type of the cache.
+      using pointer                = List::pointer;                ///< The pointer type of the cache.            
+      using const_pointer          = List::const_pointer;          ///< The const pointer type of the cache.
+      using iterator               = List::iterator;               ///< The iterator type of the cache.
+      using const_iterator         = List::const_iterator;         ///< The const iterator type of the cache.
+      using reverse_iterator       = List::reverse_iterator;       ///< The reverse iterator type of the cache.
+      using const_reverse_iterator = List::const_reverse_iterator; ///< The const reverse iterator type of the cache.
 
       /// @brief The default maximum size of the cache.
-      static constexpr std::size_t defaultMaxSize = std::numeric_limits<std::size_t>::max();
+      static constexpr size_type defaultMaxSize = std::numeric_limits<size_type>::max();
 
       /// @brief Constructs a new plan cache with the default maximum size.
       PlanCache() = default;
@@ -150,7 +83,7 @@ AFFT_EXPORT namespace afft
        * @brief Constructs a new plan cache with the specified maximum size.
        * @param maxSize The maximum number of plans that the cache can hold.
        */
-      PlanCache(std::size_t maxSize)
+      PlanCache(size_type maxSize)
       : mMaxSize{checkMaxSize(maxSize)}
       {}
 
@@ -158,7 +91,7 @@ AFFT_EXPORT namespace afft
        * @brief Constructs a new plan cache with the default maximum size and a list of plans.
        * @param plans A list of plans to insert into the cache.
        */
-      PlanCache(std::initializer_list<std::shared_ptr<Plan>> plans)
+      PlanCache(std::initializer_list<value_type> plans)
       : PlanCache{defaultMaxSize, plans}
       {}
 
@@ -167,12 +100,12 @@ AFFT_EXPORT namespace afft
        * @param maxSize The maximum number of plans that the cache can hold.
        * @param plans A list of plans to insert into the cache.
        */
-      PlanCache(std::size_t maxSize, std::initializer_list<std::shared_ptr<Plan>> plans)
+      PlanCache(size_type maxSize, std::initializer_list<value_type> plans)
       : PlanCache{maxSize}
       {
-        for (auto&& plan : plans)
+        for (auto& plan : plans)
         {
-          insert(std::move(plan));
+          insert(plan);
         }
       }
       
@@ -204,7 +137,7 @@ AFFT_EXPORT namespace afft
        * @brief Get the number of plans in the cache.
        * @return The number of plans in the cache.
        */
-      [[nodiscard]] std::size_t getSize() const noexcept
+      [[nodiscard]] size_type getSize() const noexcept
       {
         return mList.size();
       }
@@ -213,7 +146,7 @@ AFFT_EXPORT namespace afft
        * @brief Get the maximum number of plans that the cache can hold.
        * @return The maximum number of plans that the cache can hold.
        */
-      [[nodiscard]] std::size_t getMaxSize() const noexcept
+      [[nodiscard]] size_type getMaxSize() const noexcept
       {
         return mMaxSize;
       }
@@ -222,13 +155,13 @@ AFFT_EXPORT namespace afft
        * @brief Set the maximum number of plans that the cache can hold.
        * @param maxSize The maximum number of plans that the cache can hold.
        */
-      void setMaxSize(std::size_t maxSize)
+      void setMaxSize(size_type maxSize)
       {
         mMaxSize = checkMaxSize(maxSize);
 
         while (mList.size() > mMaxSize)
         {
-          mMap.erase(mList.back()->getDesc());
+          mMap.erase(mList.back()->getDescription());
           mList.pop_back();
         }
       }
@@ -247,7 +180,7 @@ AFFT_EXPORT namespace afft
        * @param plan The plan to insert.
        * @return The inserted plan.
        */
-      Plan& insert(std::shared_ptr<Plan> plan)
+      iterator insert(value_type plan)
       {
         // Check if the value is null
         if (!plan)
@@ -256,7 +189,7 @@ AFFT_EXPORT namespace afft
         }
 
         // Check if the plan has a centralized memory layout
-        if (plan->getMemoryLayout() != MemoryLayout::centralized)
+        if (plan->getDescription().getMemoryLayout() != MemoryLayout::centralized)
         {
           throw Exception{Error::invalidArgument, "Only plans with centralized memory layout can be inserted into the cache"};
         }
@@ -265,7 +198,7 @@ AFFT_EXPORT namespace afft
         if (mList.size() >= mMaxSize)
         {
           // Remove the last element from the list and the map
-          mMap.erase(mList.back()->getDesc());
+          mMap.erase(mList.back()->getDescription());
           mList.pop_back();
         }
 
@@ -273,62 +206,21 @@ AFFT_EXPORT namespace afft
         mList.emplace_front(std::move(plan));
 
         // Insert the new element into the map
-        auto [it, inserted] = mMap.emplace(mList.front()->getDesc(), mList.begin());
+        auto [it, inserted] = mMap.emplace(mList.front()->getDescription(), mList.begin());
 
         if (!inserted)
         {
           throw std::runtime_error{"Failed to insert plan into cache"};
         }
 
-        return *it->second->get();
+        return it->second;
       }
 
       /**
-       * @brief Release a plan from the cache that matches the specified parameters.
-       * @param transformParams The parameters of the transform.
-       * @param targetParams The parameters of the target.
-       * @param memoryLayout The memory layout of the plan.
-       * @return The released plan.
+       * @brief Erase a plan from the cache that matches the plan description.
+       * @param desc The description of the plan to erase.
        */
-      template<typename TransformParamsT, typename TargetParamsT>
-      [[nodiscard]] std::shared_ptr<Plan> release(const TransformParamsT&        transformParams,
-                                                  const TargetParamsT&           targetParams,
-                                                  const CentralizedMemoryLayout& memoryLayout = {})
-      {
-        static_assert(isTransformParameters<TransformParamsT>, "invalid transform parameters type");
-        static_assert(isTargetParameters<TargetParamsT>, "invalid target parameters type");
-
-        const auto desc = detail::Desc{transformParams, targetParams, memoryLayout};
-
-        if (auto mapIter = mMap.find(desc); mapIter != mMap.end())
-        {
-          auto plan = std::move(*mapIter->second);
-
-          mList.erase(mapIter->second);
-          mMap.erase(mapIter);
-
-          return plan;
-        }
-
-        throw std::runtime_error{"plan not found in cache"};
-      }
-
-      /**
-       * @brief Erase a plan from the cache that matches the specified parameters.
-       * @param transformParams The parameters of the transform.
-       * @param targetParams The parameters of the target.
-       * @param memoryLayout The memory layout of the plan.
-       */
-      template<typename TransformParamsT, typename TargetParamsT>
-      void erase(const TransformParamsT&        transformParams,
-                 const TargetParamsT&           targetParams,
-                 const CentralizedMemoryLayout& memoryLayout = {})
-      {
-        static_assert(isTransformParameters<TransformParamsT>, "invalid transform parameters type");
-        static_assert(isTargetParameters<TargetParamsT>, "invalid target parameters type");
-
-        [[maybe_unused]] auto plan = release(transformParams, targetParams, memoryLayout);
-      }
+      void erase(const Description& desc);
 
       /**
        * @brief Swap the contents of this cache with another cache.
@@ -366,22 +258,12 @@ AFFT_EXPORT namespace afft
       }
 
       /**
-       * @brief Find a plan in the cache that matches the specified parameters.
-       * @param transformParams The parameters of the transform.
-       * @param targetParams The parameters of the target.
-       * @param memoryLayout The memory layout of the plan.
+       * @brief Find a plan in the cache that matches the plan description.
+       * @param desc The description of the plan to find.
        * @return The plan if found.
        */
-      template<typename TransformParamsT, typename TargetParamsT>
-      [[nodiscard]] iterator find(const TransformParamsT&        transformParams,
-                                  const TargetParamsT&           targetParams,
-                                  const CentralizedMemoryLayout& memoryLayout = {}) const
+      [[nodiscard]] iterator find(const Description& desc)
       {
-        static_assert(isTransformParameters<TransformParamsT>, "invalid transform parameters type");
-        static_assert(isTargetParameters<TargetParamsT>, "invalid target parameters type");
-
-        const auto desc = detail::Desc{transformParams, targetParams, memoryLayout};
-
         if (auto mapIter = mMap.find(desc); mapIter != mMap.end())
         {
           auto listIter = mapIter->second;
@@ -415,16 +297,103 @@ AFFT_EXPORT namespace afft
         return iterator{mList.end()};
       }
 
+      /**
+       * @brief Get a const iterator to the first plan in the cache.
+       * @return A const iterator to the first plan in the cache.
+       */
+      [[nodiscard]] const_iterator begin() const noexcept
+      {
+        return cbegin();
+      }
+
+      /**
+       * @brief Get a const iterator to the end of the cache.
+       * @return A const iterator to the end of the cache.
+       */
+      [[nodiscard]] const_iterator end() const noexcept
+      {
+        return cend();
+      }
+
+      /**
+       * @brief Get a const iterator to the first plan in the cache.
+       * @return A const iterator to the first plan in the cache.
+       */
+      [[nodiscard]] const_iterator cbegin() const noexcept
+      {
+        return const_iterator{mList.cbegin()};
+      }
+
+      /**
+       * @brief Get a const iterator to the end of the cache.
+       * @return A const iterator to the end of the cache.
+       */
+      [[nodiscard]] const_iterator cend() const noexcept
+      {
+        return const_iterator{mList.cend()};
+      }
+
+      /**
+       * @brief Get a reverse iterator to the first plan in the cache.
+       * @return A reverse iterator to the first plan in the cache.
+       */
+      [[nodiscard]] reverse_iterator rbegin() noexcept
+      {
+        return reverse_iterator{mList.rbegin()};
+      }
+
+      /**
+       * @brief Get a reverse iterator to the end of the cache.
+       * @return A reverse iterator to the end of the cache.
+       */
+      [[nodiscard]] reverse_iterator rend() noexcept
+      {
+        return reverse_iterator{mList.rend()};
+      }
+
+      /**
+       * @brief Get a const reverse iterator to the first plan in the cache.
+       * @return A const reverse iterator to the first plan in the cache.
+       */
+      [[nodiscard]] const_reverse_iterator rbegin() const noexcept
+      {
+        return crbegin();
+      }
+
+      /**
+       * @brief Get a const reverse iterator to the end of the cache.
+       * @return A const reverse iterator to the end of the cache.
+       */
+      [[nodiscard]] const_reverse_iterator rend() const noexcept
+      {
+        return crend();
+      }
+
+      /**
+       * @brief Get a const reverse iterator to the first plan in the cache.
+       * @return A const reverse iterator to the first plan in the cache.
+       */
+      [[nodiscard]] const_reverse_iterator crbegin() const noexcept
+      {
+        return const_reverse_iterator{mList.crbegin()};
+      }
+
+      /**
+       * @brief Get a const reverse iterator to the end of the cache.
+       * @return A const reverse iterator to the end of the cache.
+       */
+      [[nodiscard]] const_reverse_iterator crend() const noexcept
+      {
+        return const_reverse_iterator{mList.crend()};
+      }
     protected:
     private:
-      
-
       /**
        * @brief Checks if the maximum size of the cache is valid.
        * @param maxSize The maximum size of the cache.
        * @return The maximum size of the cache.
        */
-      static constexpr std::size_t checkMaxSize(std::size_t maxSize)
+      static constexpr size_type checkMaxSize(size_type maxSize)
       {
         if (maxSize == 0)
         {
@@ -434,9 +403,9 @@ AFFT_EXPORT namespace afft
         return maxSize;
       }
 
-      Map         mMap{};                   ///< The map of the cache.
-      List        mList{};                  ///< The list of the cache.
-      std::size_t mMaxSize{defaultMaxSize}; ///< The maximum size of the cache.
+      Map       mMap{};                   ///< The map of the cache.
+      List      mList{};                  ///< The list of the cache.
+      size_type mMaxSize{defaultMaxSize}; ///< The maximum size of the cache.
   };  
 } // namespace afft
 
