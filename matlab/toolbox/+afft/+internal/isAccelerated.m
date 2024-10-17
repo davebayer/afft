98d10d3d146db9126c17c359c7d2cf14bdebb946
David Bayer <bayer.david99@gmail.com>
2024-10-17 11:12:06 +0200

    #1: fix `afft.internal.isAccelerated`

--- matlab/toolbox/+afft/+internal/isAccelerated.m
+++ matlab/toolbox/+afft/+internal/isAccelerated.m
@@ -1,7 +1,7 @@
 function res = isAccelerated()
   persistent cachedRes;
   if isempty(cachedRes)
-    cachedRes = isfile(strcat('afft_matlab.', mexext));
+    cachedRes = exist(strcat('+afft/+internal/afft_matlab.', mexext), 'file') == 3;
   end
 
   res = cachedRes;
